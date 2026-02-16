528edbc6b84d78c2909deaa88bfdd657690350e4
Hannahlud24 <l25170968@culiacan.tecnm.mx>
2026-02-16 10:24:57 -0700

    Add files via upload

---
+++ js/sketch.js
@@ -0,0 +1,46 @@
+function setup() {
+  createCanvas(800, 500);
+  background(180);
+ 
+ 
+ 
+}
+ 
+function draw() {
+   
+   //stroke(0);
+   //line(0, 250, 800, 250);
+   //line(400, 0, 400, 800);
+   //line(800, 500, 0, 0);
+   //line(0, 500, 800, 0);
+   //stroke(167);
+   //noStroke();
+   //fill(252, 111, 146);
+   //circle(400,250, 200);
+ 
+   stroke(0);
+   line(30,20,185,20);
+   stroke(126);
+   line(185,20,185,175);
+   stroke(255);
+   line(185,175,30,175);
+   line(30,175,30,20);
+ 
+    stroke(0);
+   line(615,20,770,20);
+   stroke(126);
+   line(770,20,770,175);
+   stroke(255);
+   line(770,175,615,175);
+   line(615,175,615,20);
+ 
+    stroke(0);
+   line(615,250,770,250);
+   stroke(126);
+   line(100,250,770,250);
+   stroke(255);
+   line(185,175,30,175);
+   line(30,175,30,20);
+ 
+ 
+}
